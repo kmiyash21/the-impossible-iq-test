9befba3911c71ca83f28094f6837da731b86e314
kmiyash21 <47133069+kmiyash21@users.noreply.github.com>
2020-04-21 12:24:38 -0700

    Level 1 counter

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,39 +1,22 @@
-//create a variable to hold one ball
-let b;
-let anotherBall;
+let angle = 45;
 
 function setup() {
-  createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+ createCanvas(1000,800);
 }
-
 
 function draw(){
-	background(220);
-    b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
-    b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
-    anotherBall.drawBall();
-    anotherBall.moveBall();
-
+  background("blue");
+  textSize(80);
+  fill("black");
+  text(+ angle,430, 600);
+  textSize(32);
+  fill("black");
+  text("Press Up Arrow to add and Down Arrow to subtract.",100,500)
 }
-
-
-//ball class from which to create new balls with similar properties.
-class Ball {
-
-	constructor(x,y,color){ //every ball needs an x value and a y value
-		    this.x = x;
-    		this.y = y;
-        this.color= color;
-	}
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
-    		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
-	}
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
-	}
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    angle = angle+1;
+  } else if (keyCode === DOWN_ARROW) {
+    angle = angle-1;
+  }
 }
