966662c651eec32a12556dbe6483fff9141780e0
kmiyash21 <47133069+kmiyash21@users.noreply.github.com>
2020-04-24 12:22:29 -0700

    Finished Level 1 and Combined Start Screen

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,22 +1,74 @@
-let angle = 45;
+var mode = 0
 
 function setup() {
- createCanvas(1000,800);
+  createCanvas(1000, 800);
 }
 
-function draw(){
+function draw() {
+  if (mode == 0) {
+    screen1();
+  } else if (mode == 1) {
+    screen2();
+  } else if (mode == 2) {
+    screen3();
+  }
+
+}
+//Useful to have multiple elements on one screen
+function screen1() {
+  background("red");
+    textSize(40);
+    text("Click Your Mouse on the Circle to Start the Game",60,200);
+    textSize(30);
+    text("The Impossible IQ Game by Annie Hockin and Kyler Miyashita",100,650)
+  fill("yellow");
+  strokeWeight(3)
+  stroke("green");
+  ellipse(width / 2, height / 2, 300, 300);
+}
+
+function screen2() {
   background("blue");
+  noStroke();
   textSize(80);
   fill("black");
-  text(+ angle,430, 600);
-  textSize(32);
+  text("45",450, 630);
+  text("30",250, 630);
+  text("60",650, 630);
+  textSize(50);
   fill("black");
-  text("Press Up Arrow to add and Down Arrow to subtract.",100,500)
+  text("Click on the right answer.",230,500)
+  fill("yellow")
+  triangle(300,200,300,400,700,400);
+  rect(300,379,20,20);
+  fill("black")
+  fill("blue")
+  strokeWeight(5);
+  stroke("black")
+  ellipse(560,580,20,20)
+  ellipse(360,580,20,20)
+  ellipse(760,580,20,20)
+  noStroke();
+  fill("black")
+  textSize(40);
+  text("Find X",450,150)
+  textSize(20)
+  text("x",310,230)
+  text("y",640,390)
 }
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    angle = angle+1;
-  } else if (keyCode === DOWN_ARROW) {
-    angle = angle-1;
-  }
+
+function screen3() {
+
+}
+
+
+//Method 1: More flexible, any order
+function mousePressed() {
+  if (mode == 0) {
+    mode = 1;
+  } else if (mode == 1) {
+    mode = 2;
+  } else if (mode == 2) {
+    mode = 0;
+  }
 }
