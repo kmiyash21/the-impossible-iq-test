9533a16ded5fbcdaa27eb85ce226f8fe881888e3
Annie Hockin <anniehockin@gmail.com>
2020-04-29 12:16:29 -0700

    started working on level 2

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,37 +1,9 @@
-//create an empty array called balls
-let balls = [];
-
-//create a variable to hold your avatar
-let me;
-
-
 function setup() {
   createCanvas(500, 400);
 
-  //make one avatar called me
-  me = new Avatar(width/2, 300, 3);
-
-}
-
 function draw(){
 	background(0);
-
-  me.drawMe();
-  me.moveMe();
-
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(random(0,width),0, -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
-    }
-
-//	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
-	  }
-
+  ellipse(100,200,50,50);
 }
 
 //avatar class
